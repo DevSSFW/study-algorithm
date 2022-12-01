3f767dd50a4e1130a1e683220d0fe58f9c4bf848
alivejuicy <kkddhh1998@gmail.com>
2022-12-01 23:56:54 +0900

    hashcode()

--- donghyun/src/Baekjoon/test.java
+++ donghyun/src/Baekjoon/test.java
@@ -1,7 +1,26 @@
 package Baekjoon;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class test {
-    public static void main(String[] args) {
-        System.out.println("테스트입니다.");
+    private final String name; // 필드
+
+    public test(String name) { // 생성자
+        this.name = name;
     }
+
+    public String getName() { // Getter Method
+        return name;
+    }
+    private static test nana = new test("nana");
+    private static test john = new test("john");
+
+    static Set<test> set = new HashSet<>(Arrays.asList(nana, john));
+
+    public static void main(String[] args) {
+
+    }
+
 }
