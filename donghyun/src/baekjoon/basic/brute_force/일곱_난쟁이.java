bd5c261f0bf5d877698c0e7f2c8e3e65a2687a8a
alivejuicy <kkddhh1998@gmail.com>
2022-12-07 01:16:00 +0900

    solve clear

--- donghyun/src/baekjoon/basic/brute_force/일곱_난쟁이.java
+++ donghyun/src/baekjoon/basic/brute_force/일곱_난쟁이.java
@@ -6,24 +6,34 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class 일곱_난쟁이 {
-    public static void main(String[] args) {
-
-
-    }
-
-    private static void getSmallThing() throws IOException {
+    public static void main(String[] args) throws IOException {
         int sum = 0;
+        int spyA = 0, spyB = 0;
         int[] smallThing = new int[9];
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         for (int index = 0; index < smallThing.length; index++) {
             smallThing[index] = Integer.parseInt(reader.readLine());
             sum += smallThing[index];
         }
         Arrays.sort(smallThing);
-    }
-    private static void searchNotSmallThing(){
-        int nota;
-        int notb;
+
+        for (int a = 0; a < smallThing.length - 1; a++) {
+            for (int b = a + 1; b < smallThing.length; b++) {
+                if (sum - smallThing[a] - smallThing[b] == 100) {
+                    spyA = a;
+                    spyB = b;
+                    break;
+                }
+            }
+        }
+        for (int j = 0; j < smallThing.length; j++) {
+            if (j == spyA || j == spyB) {
+                continue;
+            }
+            System.out.println(smallThing[j]);
+        }
+        reader.close();
     }
 }
