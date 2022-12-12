2abb394f7d4b03b6df6a6bcb44da8c899abc2522
Sangyoon <qlrqod123123@naver.com>
2022-12-12 21:03:14 +0900

    정수 N개의 합

---
+++ sangyoon/src/Baekjoon/B_15596.java
@@ -0,0 +1,13 @@
+package Baekjoon;
+
+public class B_15596 {
+   long sum(int[] a) {
+       long sum = 0;
+
+       for(int i = 0; i < a.length; i++) {
+           sum += a[i];
+       }
+
+       return sum;
+   }
+}
