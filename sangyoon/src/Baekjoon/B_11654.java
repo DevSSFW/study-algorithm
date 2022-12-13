9c21d7deb9c90bd6924786ad120c01cf145f51a1
Sangyoon <qlrqod123123@naver.com>
2022-12-14 00:54:28 +0900

    아스키 코드

---
+++ sangyoon/src/Baekjoon/B_11654.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B_11654 {
+    public static void main(String[] args) throws IOException {
+        int n = System.in.read();
+        System.out.print(n);
+    }
+}
