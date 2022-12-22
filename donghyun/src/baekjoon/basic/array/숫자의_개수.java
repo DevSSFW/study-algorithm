e52b4622d1eab4a02ba9e193820b90bfcbf46186
alivejuicy <kkddhh1998@gmail.com>
2022-12-22 23:57:28 +0900

    Baekjoon : 숫자의 개수(B_2577) solve

---
+++ donghyun/src/baekjoon/basic/array/숫자의_개수.java
@@ -0,0 +1,31 @@
+package baekjoon.basic.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+/**
+ * B_2577
+ */
+public class 숫자의_개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int multiply = 1;
+
+        for (int i = 0; i < 3; i++) {
+            multiply *= Integer.parseInt(br.readLine());
+        }
+
+        int[] counter = new int[10];
+
+        while (multiply > 0) {
+            counter[multiply % 10]++;
+            multiply /= 10;
+        }
+        for (int j = 0; j < counter.length; j++) {
+            System.out.println(counter[j]);
+        }
+        br.close();
+    }
+}
