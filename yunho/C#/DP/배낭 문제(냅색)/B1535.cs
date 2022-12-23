765c842dfbaf3a53910b86e434d6f46fb7f0d2d5
developerYHLee <dbsgh812@naver.com>
2022-12-23 23:04:11 +0900

    안녕

---
+++ yunho/C#/DP/배낭 문제(냅색)/B1535.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace BaekJoon.src
+{
+    class B1535 //안녕
+    {
+        StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        public B1535()
+        {
+            int N = int.Parse(sr.ReadLine());
+            int[] L = new int[N], J = new int[N], dp = new int[101];
+
+            int[] tokens = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            for (int i = 0; i < N; i++) L[i] = tokens[i];
+            tokens = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            for (int i = 0; i < N; i++) J[i] = tokens[i];
+
+            for(int i = 0; i < N; i++)
+            {
+                if (L[i] >= 100) continue;
+
+                for(int j = 100; j > L[i]; j--)
+                {
+                    if (j - L[i] > 0) dp[j] = Math.Max(dp[j], dp[j - L[i]] + J[i]);
+                }
+            }
+
+            sw.WriteLine(dp[100]);
+            Close_Stream();
+        }
+
+        public void Close_Stream()
+        {
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
