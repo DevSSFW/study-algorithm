014660f7ad853999517815d11d410b79012bbdb1
alivejuicy <kkddhh1998@gmail.com>
2022-12-28 15:59:23 +0900

    Baekjoon : 알파벳 개수(B_10808) solve

---
+++ donghyun/src/baekjoon/basic/array/알파벳_개수.java
@@ -0,0 +1,23 @@
+package baekjoon.basic.array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * B_10808
+ */
+public class 알파벳_개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        int[] arr = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++; //각 알파벳 index 위치에 ++
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
