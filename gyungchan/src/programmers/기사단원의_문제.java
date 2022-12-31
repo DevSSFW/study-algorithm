999a892c05f202489c95caa88e88c647009c1ef8
gyungchan Jo <namejgc@naver.com>
2022-12-31 16:44:27 +0900

    3주차 풀이 - 코드 수정

--- gyungchan/src/programmers/기사단원의_문제.java
+++ gyungchan/src/programmers/기사단원의_문제.java
@@ -1,23 +1,25 @@
 package programmers;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 기사단원의_문제 {
     public static int solution(int number, int limit, int power) {
-        int answer = 1; //1인 경우 무조건 약수가 1이므로 1의 약수값을 먼저 넣어준다.
-        for (int i = 2; i <= number; i++) {
-            //모든 수는 1과 자신과 같은 수를 약수로 가지므로 count 값을 2로 저장한다.
-            int count = 2;
-            //어떤 수든 자신의 중간값보다 더 큰 수가 약수로 나올수 없으므로 j는 i/2의 값까지만 반복한다.
-            for (int j = 2; j <= i/2; j++) {
-                if (i % j == 0) {
-                    count++;
-                }
+        int answer = 0;
+        int[] count = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            if (count > limit) {
+        }
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
                 answer += power;
             } else {
-                answer += count;
+                answer += count[i];
             }
         }
 
@@ -25,9 +27,13 @@ public class 기사단원의_문제 {
     }
 
     public static void main(String[] args) throws IOException {
-        int number = 10;
-        int limit = 3;
-        int power = 2;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int number = Integer.parseInt(st.nextToken());
+        int limit = Integer.parseInt(st.nextToken());
+        int power = Integer.parseInt(st.nextToken());
+
         System.out.println(solution(number, limit, power));
     }
 }
