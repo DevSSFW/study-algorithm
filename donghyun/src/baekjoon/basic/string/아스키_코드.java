1ded09cb207c1db76dac512ac26cdcbe88db733f
alivejuicy <kkddhh1998@gmail.com>
2023-01-05 23:41:42 +0900

    Baekjoon : 문자열 반복, 숫자의 합, 아스키 코드 solve

--- donghyun/src/baekjoon/basic/string/아스키_코드.java
+++ donghyun/src/baekjoon/basic/string/아스키_코드.java
@@ -1,7 +1,13 @@
 package baekjoon.basic.string;
 
-public class 아스키_코드 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class 아스키_코드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char c = br.readLine().charAt(0);
+
+        System.out.println((int) c);
     }
 }
