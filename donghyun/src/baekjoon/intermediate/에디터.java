f032accbf4f72d5410728c86cdfc264e31598ccd
alivejuicy <kkddhh1998@gmail.com>
2023-01-14 21:08:18 +0900

    Baekjoon : 에디터

---
+++ donghyun/src/baekjoon/intermediate/에디터.java
@@ -0,0 +1,28 @@
+package baekjoon.intermediate;
+
+import java.io.*;
+import java.util.LinkedList;
+
+public class 에디터 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        LinkedList<String> list = new LinkedList<>();
+
+        int M = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < M; i++) {
+
+
+
+            // A,B로만 이루어져있다. B가 2번만 등장
+            // N의 길이를 가진 문자열을 사전순 배치, K번째 문자열의 두번째 B의 위치?
+            //
+
+
+
+        }
+
+    }
+}
