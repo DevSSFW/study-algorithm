7d15d8cdefdf6109ea93e0f7754742b5253b137f
alivejuicy <kkddhh1998@gmail.com>
2023-01-15 16:26:19 +0900

    Baekjoon : 에디터 solve

--- donghyun/src/baekjoon/intermediate/에디터.java
+++ donghyun/src/baekjoon/intermediate/에디터.java
@@ -2,27 +2,61 @@ package baekjoon.intermediate;
 
 import java.io.*;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
+/**
+ * B_1406, 연결리스트
+ */
 public class 에디터 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        LinkedList<String> list = new LinkedList<>();
+        String str = br.readLine(); //문자열 입력받기 abcd
+        LinkedList<Character> list = new LinkedList<>();
 
-        int M = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < M; i++) {
-
-
-
-            // A,B로만 이루어져있다. B가 2번만 등장
-            // N의 길이를 가진 문자열을 사전순 배치, K번째 문자열의 두번째 B의 위치?
-            //
-
-
-
+        for (String s : str.split("")) {
+            list.add(s.charAt(0)); // [a, b, c ,d]
         }
 
+        int M = Integer.parseInt(br.readLine()); //명령 실행 행 개수
+
+        ListIterator<Character> cursor = list.listIterator();
+
+        while (cursor.hasNext()) {
+            cursor.next();
+        }
+
+        while (M-- > 0) {
+            String[] line = br.readLine().split(" ");
+
+            if (line[0].equals("L")) {
+                if (cursor.hasPrevious()) {
+                    cursor.previous();
+                }
+            } else if (line[0].equals("D")) {
+                if (cursor.hasNext()) {
+                    cursor.next();
+                }
+            } else if (line[0].equals("B")) {
+                if (cursor.hasPrevious()) {
+                    cursor.previous();
+                    cursor.remove();
+                }
+            } else if (line[0].equals("P")) {
+                cursor.add(line[1].charAt(0));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : list) {
+            sb.append(c);
+        }
+        bw.write(String.valueOf(sb));
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
