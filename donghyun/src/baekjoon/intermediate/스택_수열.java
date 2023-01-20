6b4ef563addf8bd120cccbb95d6af2c09359a6ac
alivejuicy <kkddhh1998@gmail.com>
2023-01-20 18:45:24 +0900

    Baekjoon : 스택 수열 solve

---
+++ donghyun/src/baekjoon/intermediate/스택_수열.java
@@ -0,0 +1,47 @@
+package baekjoon.intermediate;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+/**
+ * B_1874
+ */
+public class 스택_수열 {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+
+        int T = Integer.parseInt(br.readLine());
+        int max = 0;
+
+        for (int i = 0; i < T; i++) {
+
+            int number = Integer.parseInt(br.readLine());
+
+            if (number > max) {
+                for (int k = max + 1; k <= number; k++) {
+                    stack.push(k);
+                    sb.append("+").append("\n");
+                }
+                max = number;
+
+            } else if (stack.peek() != number) {
+                System.out.println("NO");
+                return;
+            }
+
+            stack.pop();
+            sb.append("-").append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
