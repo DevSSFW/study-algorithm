bb8d352a3b4e27ad561e2f62d9ed1fe06bad678f
gyungchan Jo <namejgc@naver.com>
2023-01-22 01:32:19 +0900

    7주차 풀이

---
+++ gyungchan/src/programmers/체육복.java
@@ -0,0 +1,44 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 체육복 {
+    public static int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = 0; j < reserve.length; j++) {
+                if (reserve[j] == lost[i]) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < lost.length; i++) {
+            if (lost[i] == -1) {
+                continue;
+            }
+            answer--;
+            for (int j = 0; j < reserve.length; j++) {
+                if (reserve[j] - 1 == lost[i] || reserve[j] + 1 == lost[i]) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] lost = {4, 2};
+        int[] reserve = {3, 5};
+
+        System.out.println(solution(n, lost, reserve));
+    }
+}
---
+++ gyungchan/src/programmers/평균_구하기.java
@@ -0,0 +1,17 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 평균_구하기 {
+    public static double solution(int[] arr) {
+        double answer = (double) Arrays.stream(arr).sum() / arr.length;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+
+        System.out.println(solution(arr));
+    }
+}
