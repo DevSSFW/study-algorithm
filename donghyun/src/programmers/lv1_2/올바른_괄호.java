8e016d9d7c10863e1260a3cb3c27f9d6e92161df
alivejuicy <kkddhh1998@gmail.com>
2023-01-25 17:03:50 +0900

    Programmers Lv2 : 올바른 괄호 solve

--- donghyun/src/programmers/lv1_2/올바른_괄호.java
+++ donghyun/src/programmers/lv1_2/올바른_괄호.java
@@ -1,4 +1,26 @@
 package programmers.lv1_2;
 
+
+import java.util.Stack;
+
 public class 올바른_괄호 {
+    static boolean answer;
+    public static boolean solution(String s) { // (())()()
+        Stack<Character> stack = new Stack<>();
+
+        for (int T = 0; T < s.length(); T++) {
+            if (s.charAt(T) == '(') {
+                stack.push(s.charAt(T));
+            } else {
+                if (stack.size() == 0) {
+                    stack.push(s.charAt(T));
+                    break;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        answer = stack.isEmpty();
+        return answer;
+    }
 }
