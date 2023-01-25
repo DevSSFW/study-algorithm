130aa7f4820d0a7fdaa6bcda5825e75a20bf039d
alivejuicy <kkddhh1998@gmail.com>
2023-01-25 14:01:49 +0900

    Programmers Lv1 : 문자열 다루기 기본 solve

--- donghyun/src/programmers/lv1_2/문자열_다루기_기본.java
+++ donghyun/src/programmers/lv1_2/문자열_다루기_기본.java
@@ -1,4 +1,28 @@
 package programmers.lv1_2;
 
+import java.util.Scanner;
+
 public class 문자열_다루기_기본 {
+    public static boolean solution(String s) {
+        boolean answer = true;
+
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            int st = s.charAt(i);
+            if (st <= 47 || st >= 58) {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(solution(s));
+    }
 }
