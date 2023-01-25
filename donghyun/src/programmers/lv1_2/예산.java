0c77fbd9779fb9b704ed4464617986b0d01eba69
alivejuicy <kkddhh1998@gmail.com>
2023-01-25 14:15:48 +0900

    Programmers Lv1 : 예산 solve

--- donghyun/src/programmers/lv1_2/예산.java
+++ donghyun/src/programmers/lv1_2/예산.java
@@ -1,4 +1,21 @@
 package programmers.lv1_2;
 
+import java.util.Arrays;
+
+/**
+ * int[] d , int budget
+ */
 public class 예산 {
+    public static int solution(int[] d, int budget) {
+        Arrays.sort(d); //오름차순으로 정렬
+        int answer = 0;
+
+        for (int t = 0; t < d.length; t++) {
+            if (budget >= d[t]) {
+                budget -= d[t];
+                answer++;
+            }
+        }
+        return answer;
+    }
 }
