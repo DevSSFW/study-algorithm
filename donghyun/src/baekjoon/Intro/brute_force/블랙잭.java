963c297a4198aa29e40905a631024d792318f162
alivejuicy <kkddhh1998@gmail.com>
2023-02-21 19:43:34 +0900

    백준 : 완전탐색 풀이

--- donghyun/src/baekjoon/Intro/brute_force/블랙잭.java
+++ donghyun/src/baekjoon/Intro/brute_force/블랙잭.java
@@ -1,4 +1,38 @@
 package baekjoon.Intro.brute_force;
 
+import java.io.*;
+import java.util.*;
+
 public class 블랙잭 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int ea;
+    static int max;
+
+    public static void main(String[] args) throws Exception {
+        String[] input = br.readLine().split(" ");
+        ea = Integer.parseInt(input[0]);
+        max = Integer.parseInt(input[1]);
+
+        String s = br.readLine();
+        int[] cards = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(solve(cards));
+
+    }
+
+    private static int solve(int[] cards) {
+        int sum = 0;
+        int answer = 0;
+        for (int i = 0; i < ea; i++) {
+            for (int j = i + 1; j < ea; j++) {
+                for (int k = j + 1; k < ea; k++) {
+                    sum = cards[i] + cards[j] + cards[k];
+                    if (answer < sum && sum <= max) {
+                        answer = sum;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
 }
