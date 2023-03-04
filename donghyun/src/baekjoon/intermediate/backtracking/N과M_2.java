c4ed409d736623f17eee5b0155616b17dea90914
alivejuicy <kkddhh1998@gmail.com>
2023-03-04 18:28:04 +0900

    백준 : N과 M

--- donghyun/src/baekjoon/intermediate/backtracking/N과M_2.java
+++ donghyun/src/baekjoon/intermediate/backtracking/N과M_2.java
@@ -1,4 +1,12 @@
 package baekjoon.intermediate.backtracking;
-
+import java.io.*;
+import java.util.*;
 public class N과M_2 {
+    public static int solution(){
+        return 0;
+
+    }
+    public static void main(String[] args){
+
+    }
 }
