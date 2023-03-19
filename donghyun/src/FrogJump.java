32b807ef5feb5ccad6d4b149a394a94514a63d72
alivejuicy <kkddhh1998@gmail.com>
2023-03-19 23:43:43 +0900

    FrogJump 시간 복잡도 개선

--- donghyun/src/FrogJump.java
+++ donghyun/src/FrogJump.java
@@ -1,22 +1,33 @@
 import java.util.*;
 
 public class FrogJump {
-    int answer,tmp = 0;
+    public static void main(String[] args) {
+        int result;
+        result = solution(10, 85, 30);
+        System.out.printf("result = %d, pass = %b\n", result, result == 3);
+    }
 
-    public int solution(int X, int Y, int D) {
-        while (true) {
-            if (tmp >= Y) {
-                break;
-            } else {
-                tmp += X + D;
-                answer++;
-            }
-        }
-        return answer;
+    public static int solution(int X, int Y, int D) {
+
+
+        /**
+         * solution 1 : 수학적 접근
+         */
+        /*
+        if (X == Y) // X가 Y랑 같다면
+            return 0; // 0 반환
+
+        int jumps = (Y - X) / D; // (길이 - 시작) / 점프
+        if (jumps * D + X < Y)
+            return jumps + 1;
+
+        return jumps;
+        */
+        return (int) Math.ceil((double) (Y - X) / D);
+        // ceil : 주어진 값 같거나 큰 정수중 최대값
     }
 }
 /**
  * Time Complexity : 11%
  * 개선 방안 -> while문 대신 함수적 풀이 접근
- *
  */
