09036a3551c94370dedf41b2ffcd68089816e0c3
alivejuicy <kkddhh1998@gmail.com>
2023-04-08 23:15:02 +0900

    구간합

--- donghyun/src/PermMissingElem.java
+++ donghyun/src/PermMissingElem.java
@@ -6,13 +6,13 @@ public class PermMissingElem {
         System.out.println(solution(A));
     }
 
-    private static int solution(int[] A) {
+    private static int solution(int[] A) { // A = {1, 2, 4 ,5 ,6}
         int N = A.length;
-        int maxSum = (N + 1) * (N + 2) / 2;
+        int maxSum = (N + 1) * (N + 2) / 2; //21
         int currentSum = 0;
 
         for (int i = 0; i < N; i++) {
-            currentSum += A[i];
+            currentSum += A[i]; // 18
         }
         return maxSum - currentSum;
     }
