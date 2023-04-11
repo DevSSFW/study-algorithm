c9c48924f2dab462fb2fcda533f82e6b53a9bda7
alivejuicy <kkddhh1998@gmail.com>
2023-04-11 20:42:28 +0900

    백트래킹

--- donghyun/src/baekjoon/intermediate/backtracking/N과M_2.java
+++ donghyun/src/baekjoon/intermediate/backtracking/N과M_2.java
@@ -1,12 +1,38 @@
 package baekjoon.intermediate.backtracking;
-import java.io.*;
 import java.util.*;
+
 public class N과M_2 {
-    public static int solution(){
-        return 0;
+    static int n, m; // n은 수열의 길이(1부터 n까지), m은 조합할 개수
+    static int[] arr; //조합할 숫자를 담는 배열을 생성한다.
+    static boolean[] visited; //수열의 요소를 방문 여부로 담을 것이다. (1부터 n까지)
+    static StringBuilder sb = new StringBuilder();
 
+    public static void solution(int depth, int start) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true; //T,F F -> F,T,F -> F,F,T
+            arr[depth] = i; // 1 -> 2 -> 3
+            solution(depth + 1, i + 1);
+            visited[i] = false;
+        }
     }
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[m];
+        visited = new boolean[n + 1];
+        solution(0, 1);
+        System.out.println(sb);
     }
 }
