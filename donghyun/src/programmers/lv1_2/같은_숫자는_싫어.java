66861948dcb724f12ea914162bfc828046aee8f3
alivejuicy <kkddhh1998@gmail.com>
2023-04-13 16:49:20 +0900

    같은 숫자는 싫어

--- donghyun/src/programmers/lv1_2/같은_숫자는_싫어.java
+++ donghyun/src/programmers/lv1_2/같은_숫자는_싫어.java
@@ -1,4 +1,21 @@
 package programmers.lv1_2;
 
+import java.util.*;
+
 public class 같은_숫자는_싫어 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 3, 3, 0, 1, 1};
+        System.out.println(solution(arr));
+    }
+
+    public static Stack<Integer> solution(int[] arr) {
+        Stack<Integer> answer = new Stack<>();
+        answer.push(arr[0]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (answer.peek() == arr[i]) continue;
+            answer.push(arr[i]);
+        }
+        return answer;
+    }
 }
