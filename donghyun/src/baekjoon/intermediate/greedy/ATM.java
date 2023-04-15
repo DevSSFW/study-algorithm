8ad8960aa92ef0765b239d524034f318a04bcaf9
alivejuicy <kkddhh1998@gmail.com>
2023-04-16 02:37:43 +0900

    그리디

--- donghyun/src/baekjoon/intermediate/greedy/ATM.java
+++ donghyun/src/baekjoon/intermediate/greedy/ATM.java
@@ -1,4 +1,31 @@
 package baekjoon.intermediate.greedy;
 
+import java.util.*;
+
 public class ATM {
+    static int N;
+    static int arr[];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(greedy(arr));
+
+    }
+
+    private static int greedy(int[] arr) {
+        Arrays.sort(arr);
+        int sum = 0;
+        int add = 0;
+        for (int i = 0; i < N; i++) {
+            add += arr[i];
+            sum += add;
+        }
+        return sum;
+    }
 }
