a93c165ea7b2c87f51555eb253d2ff694d850ff5
alivejuicy <kkddhh1998@gmail.com>
2023-06-07 23:45:13 +0900

    그리디

---
+++ donghyun/src/baekjoon/intermediate/greedy/회의실_배정.java
@@ -0,0 +1,31 @@
+package baekjoon.intermediate.greedy;
+
+import java.util.*;
+import java.io.*;
+
+public class 회의실_배정 {
+    static int N;
+    static int[][] arr;
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
+            }
+            return o1[0] - o2[0];
+        });
+
+    }
+}
